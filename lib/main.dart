1744973ebd8195269dbfa989a0153288e0d4bfad
Glory <malamshaglory@gmail.com>
2023-03-24 18:33:14 +0300

    bar chart and column chart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,14 +28,31 @@ class FirstRoute extends StatelessWidget {
         title: const Text('First Route'),
       ),
       body: Center(
-        child: ElevatedButton(
-          child: const Text('Open Route'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const SecondRoute()),
-            );
-          },
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: const Text('Open Route to second route'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SecondRoute()),
+                );
+              },
+            ),
+            const SizedBox(height: 10,),
+            ElevatedButton(
+              child: const Text('Third Page'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Third()),
+
+                );
+              },
+            ),
+            const SizedBox(height: 10,),
+            const BarChart(),
+                ],
         ),
       ),
     );
@@ -51,16 +70,158 @@ class SecondRoute extends StatelessWidget {
         title: const Text('Second Route'),
       ),
       body: Center(
-        child: ElevatedButton(
-          style: const ButtonStyle(
-            backgroundColor: MaterialStatePropertyAll(Colors.green),
-          ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go Back'),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 10,),
+            ElevatedButton(
+              style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.green),
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Go Back'),
+            ),
+            const SizedBox(height: 10,),
+            const SalesChart(),
+          ],
+        ),
+
+      ),
+    );
+  }
+}
+
+class SalesChart extends StatefulWidget {
+  const SalesChart({Key? key}) : super(key: key);
+
+  @override
+  State<SalesChart> createState() => _SalesChartState();
+}
+
+class _SalesChartState extends State<SalesChart> {
+
+  late List<SalesData> _chartData;
+
+  @override
+  void initState() {
+    _chartData = getChartData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(width: 400,
+        height: 300,
+        decoration: BoxDecoration(border: Border.all(width: 5.0, color: Colors.black)),
+        child: SfCartesianChart(
+            series: <ChartSeries>[ColumnSeries<SalesData, double>(dataSource: _chartData,
+                xValueMapper: (SalesData sales, _) => sales.year,
+                yValueMapper: (SalesData sales, _) => sales.sales)],
+        ),);
+  }
+
+  List<SalesData> getChartData() {
+    final List<SalesData> chartData = [
+      SalesData(2017, 25),
+      SalesData(2018, 12),
+      SalesData(2019, 24),
+      SalesData(2020, 18),
+      SalesData(2021, 30)
+    ];
+    return chartData;
+  }
+}
+
+class SalesData {
+  SalesData(this.year, this.sales);
+  final double year;
+  final double sales;
+}
+
+class Third extends StatelessWidget {
+  const Third({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.red,
+        title: const Text('Third page'),
+      ),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('hi'),
+            ElevatedButton(
+              style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.red),
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Go Back'),
+            ),
+
+          ],
         ),
       ),
     );
   }
+}
+
+class BarChart extends StatefulWidget {
+  const BarChart({Key? key}) : super(key: key);
+
+  @override
+  State<BarChart> createState() => _BarChartState();
+}
+
+class _BarChartState extends State<BarChart> {
+
+  late List<GDPData> _chartData;
+
+  @override
+  void initState() {
+    _chartData = getChartData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(width: 400, 
+            height: 300, 
+            decoration: BoxDecoration(border: Border.all(width: 5.0, color: Colors.black)),
+            child: SfCartesianChart(
+              series:  <ChartSeries>[
+                BarSeries<GDPData, String>(dataSource: _chartData,
+                xValueMapper: (GDPData gdp, _) => gdp.continent,
+                yValueMapper: (GDPData gdp, _) => gdp.gdp,),
+                
+                
+
+              ],
+              primaryXAxis: CategoryAxis(),
+            ),);
+            
+
+  }
+
+  List<GDPData> getChartData() {
+    final List<GDPData> chartData = [
+      GDPData('Oceania', 1600),
+      GDPData('Africa', 2490),
+      GDPData('S America', 2900),
+      GDPData('Europe', 23050),
+      GDPData('N America', 24880),
+      GDPData('Asia', 34390),
+    ];
+    return chartData;
+  }
+
+}
+
+class GDPData {
+  GDPData(this.continent, this.gdp);
+  final String continent;
+  final double gdp;
 }
